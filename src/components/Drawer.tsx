ff99fe45046c4b69424812cdd215e50ece03c9e9
t-uesawa <t.uesawa.works@gmail.com>
2024-06-13 13:57:59 +0900

    20240613

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,6 +1,6 @@
 // import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { Icon } from '@mui/material';
+import { CssBaseline, Icon, Typography, } from '@mui/material';
 import Home from './Home';
 import { Box, IconButton } from '@mui/material';
 
@@ -19,15 +19,27 @@ export const Drawer = (props: Props) => {
 				onClose={props.onDrawerOpen}
 				onOpen={props.onDrawerOpen}
 			>
-				<Box sx={{ p: 1, display: 'flex', justifyContent: 'space-between' }}>
-					<IconButton aria-label="close" size="small" onClick={props.onDrawerOpen}>
+				<Box sx={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					position: 'sticky',
+					top: 0,
+					zIndex: 1000,
+					p: 1,
+					bgcolor: 'background.paper',
+				}}>
+					<CssBaseline />
+					<IconButton aria-label="close" onClick={props.onDrawerOpen}>
 						<Icon>highlight_off</Icon>
 					</IconButton>
-					<IconButton aria-label="close" size="small">
+					<Typography variant='h6'>新規登録</Typography>
+					<IconButton aria-label="close">
 						<Icon>save</Icon>
 					</IconButton>
 				</Box>
-				<Home date={props.dateStr}></Home>
+				<Box sx={{ p: 1 }}>
+					<Home date={props.dateStr} />
+				</Box>
 			</SwipeableDrawer>
 		</>
 	);
